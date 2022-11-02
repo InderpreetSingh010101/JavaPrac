ed19741263223447b61018cd802a1fb060cc10cb
ips <inder17375@gmail.com>
2022-11-02 13:45:17 +0530

    sec

--- recDp.java
+++ recDp.java
@@ -21,11 +21,32 @@ public class recDp{
 
     }
 
+    public static int fibotabulation(int N , int dp[]){
+        for(int n = 0 ; n <= N ; n++){
+            if( n <= 1){
+        dp[n] = n ;
+        continue ;
+       }
+
+       if(dp[n] != 0 )
+        continue;
+
+       int ans = fibomemo(n-1 , dp) + fibomemo(n-2 , dp) ;
+       dp[n] = ans ;
+        // continue can be skipped as it at the end of loop
+        }
+
+        return dp[N] ;
+    }
+
      public static void main(String[] args) {
 
-        int  n = 6 ;
-        int[] dp = new int[6] ;
-		int ans =fibomemo( n-1 , dp) ;
+        int  n = 8 ;
+        int[] dp = new int[n] ;
+		int ans =fibotabulation( n-1 , dp) ;
         display(dp) ;
+
+        System.out.println() ;
+        System.out.print(ans) ;
 	}
 }
